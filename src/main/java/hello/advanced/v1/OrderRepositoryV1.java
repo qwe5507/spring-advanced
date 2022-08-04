c10fa7287d32fd0e39d34697976e9b22987df9bf
qwe5507 <apvmffkdls@gmail.com>
2022-08-04 22:45:55 +0900

    로그 추적기 V1 - 적용

---
+++ src/main/java/hello/advanced/v1/OrderRepositoryV1.java
@@ -0,0 +1,39 @@
+package hello.advanced.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+
+            //저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;// 요구조건의 비즈니스의 흐름을 바꾸면 안되기때문에 그대로 밖으로 던진다.
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advanced/v1/OrderServiceV1.java
@@ -0,0 +1,25 @@
+package hello.advanced.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;// 요구조건의 비즈니스의 흐름을 바꾸면 안되기때문에 그대로 밖으로 던진다.
+        }
+    }
+}
